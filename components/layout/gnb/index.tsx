581ea83f914cdb05da403a4ab27e653a73861c2e
42KIM <ksahoo524@gmail.com>
2023-04-16 17:45:46 +0900

    style: gnb, main에 기본 좌우 패딩 추가

--- components/layout/gnb/index.tsx
+++ components/layout/gnb/index.tsx
@@ -23,7 +23,7 @@ export const menus: Menu[] = [
 const Gnb = () => {
   return (
     <header className="absolute w-screen flex h-20 justify-center bg-blue-200">
-      <div className="flex w-full max-w-2xl justify-between">
+      <div className="flex w-full max-w-2xl justify-between p-5">
         <div className="flex items-center gap-10">
           {menus.map(({ name, path }) => (
             <Link key={name} href={path}>
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children, showGnb = true }: PropsWithChildren<LayoutProps>) =>
   return (
     <>
       {showGnb && <Gnb />}
-      <main className="max-w-2xl h-full m-auto pt-20">
+      <main className="max-w-2xl h-full m-auto pt-20 px-5">
         {children}
       </main>
     </>
