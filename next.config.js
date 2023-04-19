713dd50c6c5615b47f8de7a45bbcd68698dea463
42KIM <ksahoo524@gmail.com>
2023-04-19 17:57:44 +0900

    feat: nextConfig에 NODE_ENV 환경변수 추가

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
+    BASE_URL: process.env.NODE_ENV === 'development' && 'http://localhost:3000' ||
+      process.env.NODE_ENV === 'production' && process.env.PRODUCTION_URL,
     MONGODB_URI: process.env.MONGODB_URI,
   }
 };
