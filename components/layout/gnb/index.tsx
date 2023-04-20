3d893d4f49da0d15cf2a1c4fdbff867ce778b857
42KIM <ksahoo524@gmail.com>
2023-04-20 23:01:23 +0900

    feat: 임시 어드민 글쓰기 기능을 gnb로 이동시키는 로직 수정

--- components/layout/gnb/index.tsx
+++ components/layout/gnb/index.tsx
@@ -3,33 +3,49 @@ import Link from 'next/link';
 type Menu = {
   name: string,
   path: string,
+  adminOnly: boolean,
 };
 
 export const menus: Menu[] = [
   {
     name: 'Home',
     path: '/',
+    adminOnly: false,
   },
   {
     name: 'About',
     path: '/about',
+    adminOnly: false,
   },
   {
     name: 'Posts',
     path: '/posts',
+    adminOnly: false,
+  },
+  {
+    name: '글쓰기',
+    path: '/posts/create',
+    adminOnly: true,
   },
 ];
 
 const Gnb = () => {
+  //TODO - 인증 추가
+  const isAdmin = true;
+
   return (
     <header className="absolute w-screen flex h-20 justify-center bg-blue-200">
       <div className="flex w-full max-w-2xl justify-between p-5">
         <div className="flex items-center gap-10">
-          {menus.map(({ name, path }) => (
-            <Link key={name} href={path}>
-              <span>{name}</span>
-            </Link>
-          ))}
+          {menus.map(({ name, path, adminOnly }) => {
+            if (!adminOnly || (adminOnly && isAdmin)) {
+              return (
+                <Link key={name} href={path}>
+                  <span>{name}</span>
+                </Link>
+              );
+            }
+          })}
         </div>
       </div>
     </header>
--- components/pages/posts/index.tsx
+++ components/pages/posts/index.tsx
@@ -1,12 +1,8 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 const Posts = ({ results }) => {
-  const router = useRouter();
-
   return (
     <div>
-      <button onClick={() => router.push('/posts/create')}>글쓰기</button>
       <ul className='flex flex-col gap-4'>
         {results.map(({ _id, title }) => (
           <Link key={_id} href={`/posts/${_id}`}>
