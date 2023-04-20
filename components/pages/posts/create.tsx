4af0b017b07382caf9eb302ae90d5e010f64aaf8
42KIM <ksahoo524@gmail.com>
2023-04-20 17:27:10 +0900

    feat: 포스트에 카테고리 필드 추가

--- components/pages/posts/create.tsx
+++ components/pages/posts/create.tsx
@@ -11,6 +11,7 @@ const TuiEditor = dynamic(
 const CreatePost = () => {
   const [ title, setTitle ] = useState('');
   const [ date, setDate ] = useState('');
+  const [ category, setCategory ] = useState('');
   const [ tags, setTags ] = useState('');
   const editorRef = useRef<Editor>(null);
 
@@ -20,6 +21,7 @@ const CreatePost = () => {
     await APIService.createPosts({
       title,
       date,
+      category,
       content: editorRef.current?.getInstance().getHTML(),
       tags: tags.trim().split(' '),
     });
@@ -32,6 +34,9 @@ const CreatePost = () => {
       }} />
       <input className='w-3/12 border-2 p-2' type="date" onChange={(e) => {
         setDate(e.target.value);
+      }} />
+      <input className='h-10 text-md border-2 p-2' placeholder="카테고리" value={category} onChange={(e) => {
+        setCategory(e.target.value);
       }} />
       <TuiEditor editorRef={editorRef} />
       <input className='h-10 text-md border-2 p-2' placeholder="태그: 공백으로 구분" value={tags} onChange={(e) => {
