0e7c1d3559fe30160c689477cba80bfb9cc8127a
42KIM <ksahoo524@gmail.com>
2023-04-22 22:38:01 +0900

    feat: access token 쿠키를 이용하여 user info 가져오기

--- components/pages/auth/Callback.tsx
+++ components/pages/auth/Callback.tsx
@@ -8,11 +8,14 @@ const AuthCallback = () => {
   useEffect(() => {
     if (!query.code) return;
 
-    const getToken = async () => {
+    const login = async () => {
       await APIService.createToken({ code: query.code });
+      const userInfo = await APIService.getUser();
+      // TODO - 글로벌 스토어로 유저 세션 관리
+      // TODO - destination으로 돌려보내기
       push('/');
     };
-    getToken();
+    login();
   }, [ query.code, push ]);
 
   return <div>Authentication Processing...</div>;
