9b47b762833d6f702ea92c53ae236802f94c95c4
42KIM <ksahoo524@gmail.com>
2023-04-25 18:49:09 +0900

    feat: 글쓰기 페이지 비로그인 접근 시 메인페이지로 돌려보내기 (in server)

--- pages/posts/create.tsx
+++ pages/posts/create.tsx
@@ -1,5 +1,19 @@
-export {
-  default,
-} from '@/components/pages/posts/Create';
+export { default } from '@/components/pages/posts/Create';
 
-// TODO - private route
+// TODO - private route 모듈화
+export const getServerSideProps = (context) => {
+  const { req } = context;
+
+  if (!req.headers.cookie) {
+    return {
+      redirect: {
+        statusCode: 302,
+        destination: '/',
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
