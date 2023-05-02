e9cd63b58cc1918d92cf2b2e24730f1090982916
42KIM <ksahoo524@gmail.com>
2023-05-02 13:35:09 +0900

    feat: 포스트 삭제 버튼 노출 여부 수정

--- components/pages/posts/detail/index.tsx
+++ components/pages/posts/detail/index.tsx
@@ -1,4 +1,5 @@
 import { APIService } from '@/apis';
+import { useUser } from '@/lib/auth.service';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -11,6 +12,7 @@ const EditorViewer = dynamic(
 const PostDetail = (props) => {
   const { post } = props;
   const router = useRouter();
+  const user = useUser();
 
   const handleClick = async () => {
     await APIService.deletePosts({ _id: post._id });
@@ -21,7 +23,7 @@ const PostDetail = (props) => {
   return (
     <div>
       <Link href={'/posts'}>Back</Link>
-      <button onClick={handleClick}>삭제</button>
+      {user?.isAdmin && <button onClick={handleClick}>삭제</button>}
       <div>
         <span>{post.title}</span>
         <div>
