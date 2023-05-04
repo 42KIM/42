3f5ed16ade5468e1fb5f26d54a3a8aa8f2d1ddb9
42KIM <ksahoo524@gmail.com>
2023-05-04 17:25:50 +0900

    style: Post Detail 페이지 스타일 적용

--- components/pages/posts/post/Detail.tsx
+++ components/pages/posts/post/Detail.tsx
@@ -10,12 +10,20 @@ const EditorViewer = dynamic(
   { ssr: false },
 );
 
-const PostDetail = ({ post }: PostDetailProps) => {
+const PostDetail = ({ post: { date, title, content, tags } }: PostDetailProps) => {
   return (
-    <div className='py-2'>
-      <span>{post.title}</span>
+    <div className='py-4'>
+      <div className='flex flex-col items-center border-dashed border-2 border-x-0 mb-16 py-6'>
+        {tags.length > 0 && (
+          <div className='flex gap-2'>
+            {tags.map((tag) => <div key={tag} className='px-1.5 rounded-md bg-blue-100 text-sm text-blue-500'>{tag}</div>)}
+          </div>
+        )}
+        <span className='text-4xl font-bold mt-1'>{title}</span>
+        <span className='text-xs'>{date}</span>
+      </div>
       <div>
-        <EditorViewer content={post.content} />
+        <EditorViewer content={content} />
       </div>
     </div>
   );
