e89e58031620c93b5d49876e756b84eec8dbf6bf
42KIM <ksahoo524@gmail.com>
2023-05-05 23:58:41 +0900

    style: 포스트 목록 스타일 개선

--- components/pages/posts/index.tsx
+++ components/pages/posts/index.tsx
@@ -1,12 +1,28 @@
+import type { Post } from '@/models/Posts';
 import Link from 'next/link';
 
-const Posts = ({ results }) => {
+type PostsProps = {
+  posts: Array<Post>,
+};
+
+const Posts = ({ posts }: PostsProps) => {
   return (
     <div>
       <ul className='flex flex-col gap-4'>
-        {results.map(({ _id, title }) => (
-          <Link key={_id} href={`/posts/${_id}`}>
-            <li className='border-2 p-8'>✅ {title}</li>
+        {posts.map(({ _id, title, date, category, tags }) => (
+          <Link key={String(_id)} href={`/posts/${_id}`}>
+            <li className='relative flex flex-col gap-1 tems-center px-8 py-10 shadow-md hover:shadow-xl hover:-translate-y-1'>
+              <div className='flex gap-2 items-center'>
+                <span className='text-sm'>{date}</span>
+                {category && <div className='text-sm border-2 rounded-xl border-blue-300 text-sky-400 px-2'>{category}</div>}
+              </div>
+              <span className='text-3xl overflow-hidden whitespace-nowrap text-ellipsis'>{title}</span>
+              {tags.length > 0 && (
+                <div className='flex gap-2'>
+                  {tags.map((tag) => <div key={tag} className='text-sm text-blue-800'>#{tag}</div>)}
+                </div>
+              )}
+            </li>
           </Link>
         ))}
       </ul>
