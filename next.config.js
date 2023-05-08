9b6c873929609292d8fde1675c4674b8c05fe92b
42KIM <ksahoo524@gmail.com>
2023-05-08 10:45:04 +0900

    feat: localhost에서 github oauth 사용을 위해 dev 계정 추가

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
     BASE_URL: process.env.NODE_ENV === 'development' && 'http://localhost:3000' ||
       process.env.NODE_ENV === 'production' && process.env.PRODUCTION_URL,
     MONGODB_URI: process.env.MONGODB_URI,
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_ID: process.env.NODE_ENV === 'development' && process.env.GITHUB_CLIENT_ID_DEVELOPMENT ||
+      process.env.NODE_ENV === 'production' && process.env.GITHUB_CLIENT_ID_PRODUCTION,
   }
 };
 
--- pages/api/auth/token-create.ts
+++ pages/api/auth/token-create.ts
@@ -1,6 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { axiosInstance as _request } from '@/lib/axios';
 
+const CLIENT_ID = process.env.NODE_ENV === 'development' && process.env.GITHUB_CLIENT_ID_DEVELOPMENT ||
+  process.env.NODE_ENV === 'production' && process.env.GITHUB_CLIENT_ID_PRODUCTION;
+
+const CLIENT_SECRET = process.env.NODE_ENV === 'development' && process.env.GITHUB_CLIENT_SECRET_DEVELOPMENT ||
+  process.env.NODE_ENV === 'production' && process.env.GITHUB_CLIENT_SECRET_PRODUCTION;
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
 
@@ -11,8 +17,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       method: 'POST',
       url: 'https://github.com/login/oauth/access_token',
       data: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
         code: String(code),
       },
     });
@@ -20,7 +26,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const parsedToken = accessToken.split('&')[0].split('=')[1];
 
     // TODO - 쿠키 설정 (expire, secure, domain ...)
-    res.setHeader('Set-Cookie', `token=${parsedToken}; path=/`);
+    res.setHeader('Set-Cookie', `token=${parsedToken}; path=/;`);
     res.status(200).end();
   }
 }
