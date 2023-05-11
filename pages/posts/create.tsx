9f09a7d1d4deae67ea5bd75616ef6b817f55872d
42KIM <ksahoo524@gmail.com>
2023-05-12 00:08:53 +0900

    feat: 포스트 생성 페이지에 private route 모듈 적용

--- pages/posts/create.tsx
+++ pages/posts/create.tsx
@@ -1,19 +1,9 @@
+import { createPrivateGetServerSideProps } from '@/lib/create-private-get-server-side-props';
+
 export { default } from '@/components/pages/posts/Create';
 
-// TODO - private route 모듈화
-export const getServerSideProps = (context) => {
-  const { req } = context;
-
-  if (!req.headers.cookie) {
-    return {
-      redirect: {
-        statusCode: 302,
-        destination: '/',
-      },
-    };
-  }
-
+export const getServerSideProps = createPrivateGetServerSideProps(() => {
   return {
     props: {},
   };
-};
+});
