92f8fdb1a01acc523b0fadb05c41033776a39fe6
42KIM <ksahoo524@gmail.com>
2023-05-12 11:12:10 +0900

    chore: db query에 타입 할당

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,5 @@
 import dbConnect from '@/lib/mongoose';
+import type { Post } from '@/models/Posts';
 import Posts from '@/models/Posts';
 import { ObjectId } from 'mongodb';
 
@@ -7,7 +8,7 @@ export { default } from '@/components/pages/posts/post';
 export const getStaticPaths = async () => {
   await dbConnect();
 
-  const results = await Posts.find({});
+  const results = await Posts.find<Post>({});
 
   return {
     paths: results.map(({ _id }) => ({ params: { id: _id.toString() } })),
@@ -19,7 +20,7 @@ export const getStaticProps = async (context) => {
   await dbConnect();
 
   const { id } = context.params;
-  const result = await Posts.findOne({ _id: new ObjectId(id) });
+  const result = await Posts.findOne<Post>({ _id: new ObjectId(id) });
 
   return {
     props: {
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,4 +1,5 @@
 import dbConnect from '@/lib/mongoose';
+import type { Post } from '@/models/Posts';
 import Posts from '@/models/Posts';
 
 export { default } from '@/components/pages/posts';
@@ -6,7 +7,7 @@ export { default } from '@/components/pages/posts';
 export const getStaticProps = async () => {
   await dbConnect();
 
-  const results = await Posts.find({})
+  const results = await Posts.find<Post>({})
     .limit(10);
 
   return {
