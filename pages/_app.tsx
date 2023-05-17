f562bda93ae17e12b446c7ec583dfc42533f1e21
42KIM <ksahoo524@gmail.com>
2023-05-17 14:50:37 +0900

    feat: _app에 getPageProps 인터페이스 추가 및 meta 태그 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,16 +7,22 @@ import { RecoilRoot } from 'recoil';
 
 type PageComponentWithProps = NextPage & {
   getLayoutProps?: () => Parameters<typeof Layout>[0],
+  getPageProps?: () => {
+    pageTitle?: string,
+    pageDescription?: string,
+  },
 };
 
 export default function App({ Component, pageProps }: AppProps & { Component: PageComponentWithProps }) {
   const layoutProps = Component.getLayoutProps?.() || {};
 
+  const { pageTitle, pageDescription } = Component.getPageProps?.() || {};
+
   return (
     <>
       <Head>
-        <title>42</title>
-        <meta name="description" content="Logs of a front-end developer 42KIM" />
+        <title>{pageTitle ? `${pageTitle} - 42's blog` : '42\'s blog'}</title>
+        <meta name="description" content={pageDescription || '프론트엔드 개발자 42KIM의 개인 기술 블로그'} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
