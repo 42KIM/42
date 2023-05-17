607d3fb2aa0d38e3d97880a32dda5613ff68de33
42KIM <ksahoo524@gmail.com>
2023-05-17 14:51:08 +0900

    feat: Post Detail 페이지에 props 기반 title metatag 추가

--- components/pages/posts/post/Detail.tsx
+++ components/pages/posts/post/Detail.tsx
@@ -1,5 +1,6 @@
 import type { Post } from '@/models/Posts';
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
 type PostDetailProps = {
   post: Post,
@@ -12,20 +13,25 @@ const EditorViewer = dynamic(
 
 const PostDetail = ({ post: { date, title, content, tags } }: PostDetailProps) => {
   return (
-    <div className='py-4'>
-      <div className='flex flex-col items-center border-dashed border-2 border-x-0 mb-16 py-6'>
-        {tags.length > 0 && (
-          <div className='flex gap-2'>
-            {tags.map((tag) => <div key={tag} className='px-1.5 rounded-md bg-blue-100 text-sm text-blue-500'>{tag}</div>)}
-          </div>
-        )}
-        <span className='text-4xl font-bold mt-1'>{title}</span>
-        <span className='text-xs'>{date}</span>
+    <>
+      <Head>
+        <title>{`${title} - 42's blog`}</title>
+      </Head>
+      <div className='py-4'>
+        <div className='flex flex-col items-center border-dashed border-2 border-x-0 mb-16 py-6'>
+          {tags.length > 0 && (
+            <div className='flex gap-2'>
+              {tags.map((tag) => <div key={tag} className='px-1.5 rounded-md bg-blue-100 text-sm text-blue-500'>{tag}</div>)}
+            </div>
+          )}
+          <span className='text-4xl font-bold mt-1'>{title}</span>
+          <span className='text-xs'>{date}</span>
+        </div>
+        <div>
+          <EditorViewer content={content} />
+        </div>
       </div>
-      <div>
-        <EditorViewer content={content} />
-      </div>
-    </div>
+    </>
   );
 };
 
