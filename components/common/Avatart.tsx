18adcb4dc62a81118d9de0810d82aa831803ef3c
42KIM <ksahoo524@gmail.com>
2023-05-18 14:33:05 +0900

    feat: Avatar 공통 컴포넌트 추가

---
+++ components/common/Avatart.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+type AvatarProps = {
+  src: string,
+  alt?: string,
+  width: number,
+  height?: number,
+};
+
+const Avatar = ({ src, alt = 'avatar', width, height }: AvatarProps) => {
+  return (
+    <div className='rounded-full overflow-hidden'>
+      <Image src={src} alt={alt} width={width} height={height || width} />
+    </div>
+  );
+};
+
+export default Avatar;
