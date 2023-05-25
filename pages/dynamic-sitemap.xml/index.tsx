03428c3caafef3e93b53a109cb46ade96c3d79b8
42KIM <ksahoo524@gmail.com>
2023-05-26 00:57:11 +0900

    feat: 포스팅 페이지들을 위한 dynamic sitemap generate 페이지 추가

---
+++ pages/dynamic-sitemap.xml/index.tsx
@@ -0,0 +1,22 @@
+import dbConnect from '@/lib/mongoose';
+import { getServerSideSitemapLegacy } from 'next-sitemap';
+import type { Post } from '@/models/Posts';
+import Posts from '@/models/Posts';
+import type { GetServerSideProps } from 'next';
+
+const siteUrl = process.env.PRODUCTION_URL || 'https://42blog.vercel.app';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  await dbConnect();
+
+  const results = await Posts.find<Post>({});
+
+  const fields = results.map(({ _id }) => ({
+    loc: `${siteUrl}/posts/${_id.toString()}`,
+    lastmod: new Date().toISOString(),
+  }));
+
+  return getServerSideSitemapLegacy(ctx, fields);
+};
+
+export default function SiteMap() {}
