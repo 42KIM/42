5a8409e59c7294a5ad15c85fa90a86ec8ae87073
42KIM <ksahoo524@gmail.com>
2023-05-26 17:53:02 +0900

    style: 포스트 목록 텍스트 크기 및 색상 수정

--- components/pages/posts/index.tsx
+++ components/pages/posts/index.tsx
@@ -13,10 +13,10 @@ const Posts = ({ posts }: PostsProps) => {
           <Link key={String(_id)} href={`/posts/${_id}`}>
             <li className='relative flex flex-col gap-1 tems-center px-8 py-10 shadow-md hover:shadow-xl hover:-translate-y-1'>
               <div className='flex gap-2 items-center'>
-                <span className='text-sm'>{date}</span>
+                <span className='text-sm text-blue-400'>{date}</span>
                 {category && <div className='text-sm border-2 rounded-xl border-blue-300 text-sky-400 px-2'>{category}</div>}
               </div>
-              <span className='text-3xl overflow-hidden whitespace-nowrap text-ellipsis'>{title}</span>
+              <span className='text-2xl overflow-hidden whitespace-nowrap text-ellipsis'>{title}</span>
               {tags.length > 0 && (
                 <div className='flex gap-2'>
                   {tags.map((tag) => <div key={tag} className='text-sm text-blue-800'>#{tag}</div>)}
