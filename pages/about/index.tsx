568417b1b527fe91da461339cca75ebf30f7373b
42KIM <ksahoo524@gmail.com>
2023-05-28 23:02:37 +0900

    feat: markedjs를 이용해 이력서 md 파일을 html로 파싱하여 SSG

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,3 +1,15 @@
-export {
-  default,
-} from '@/components/pages/about';
+import fs from 'fs/promises';
+import { marked } from 'marked';
+
+export { default } from '@/components/pages/about';
+
+export const getStaticProps = async () => {
+  const resumeMarkdown = await fs.readFile('resume.md', 'utf-8');
+  const resumeHTML = marked(resumeMarkdown);
+
+  return {
+    props: {
+      content: resumeHTML,
+    },
+  };
+};
