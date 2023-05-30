22cc55ac143e18970af340f7ee73de28c390592f
42KIM <ksahoo524@gmail.com>
2023-05-30 19:08:46 +0900

    style: View 내부 코드블럭 텍스트 사이즈 확대

--- components/common/Viewer.tsx
+++ components/common/Viewer.tsx
@@ -3,7 +3,9 @@ type ViewerProps = {
 };
 
 const Viewer = ({ html }: ViewerProps) => {
-  return <div className="prose prose-sm prose-p:my-1 prose-hr:my-5 prose-ul:my-1" dangerouslySetInnerHTML={{ __html: html }} />;
+  return <div
+    className="prose prose-sm prose-p:my-1 prose-hr:my-5 prose-ul:my-1 prose-code:text-xs"
+    dangerouslySetInnerHTML={{ __html: html }} />;
 };
 
 export default Viewer;
