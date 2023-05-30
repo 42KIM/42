bf3db2b87edb4f1637e809a3e2d67666bc4a4907
42KIM <ksahoo524@gmail.com>
2023-05-31 00:33:25 +0900

    feat: post edit 페이지 추가에 따른 페이지 생성 및 [id] 라우트 구조 수정

---
+++ pages/posts/[id]/edit.tsx
@@ -0,0 +1,30 @@
+import { createPrivateGetServerSideProps } from '@/lib/create-private-get-server-side-props';
+import dbConnect from '@/lib/mongoose';
+import type { Post } from '@/models/Posts';
+import Posts from '@/models/Posts';
+import { ObjectId } from 'mongodb';
+
+export { default } from '@/components/pages/posts/post/Edit';
+
+export const getServerSideProps = createPrivateGetServerSideProps(async (context) => {
+  await dbConnect();
+
+  const { params } = context;
+
+  if (!params?.id || typeof params.id !== 'string') {
+    return {
+      notFound: true,
+    };
+  }
+
+  const { id } = params;
+
+  // TODO - error handle (유효하지 않은 id 일때 에러 발생 등)
+  const result = await Posts.findOne<Post>({ _id: new ObjectId(id) });
+
+  return {
+    props: {
+      post: JSON.parse(JSON.stringify(result)),
+    },
+  };
+});
