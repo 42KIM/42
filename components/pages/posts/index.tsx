b62f578e981230f1cd176031f3a553bb04183d6d
42KIM <ksahoo524@gmail.com>
2023-06-01 16:05:38 +0900

    fix: lighthouse 결과 기반, 접근성 향상을 위한 li 태그 위치 수정

--- components/pages/posts/index.tsx
+++ components/pages/posts/index.tsx
@@ -10,8 +10,8 @@ const Posts = ({ posts }: PostsProps) => {
     <div>
       <ul className='flex flex-col gap-4'>
         {posts.map(({ _id, title, date, category, tags }) => (
-          <Link key={String(_id)} href={`/posts/${_id}`}>
-            <li className='relative flex flex-col gap-1 tems-center px-8 py-10 shadow-md hover:shadow-xl hover:-translate-y-1'>
+          <li key={String(_id)} >
+            <Link href={`/posts/${_id}`} className='relative flex flex-col gap-1 tems-center px-8 py-10 shadow-md hover:shadow-xl hover:-translate-y-1'>
               <div className='flex gap-2 items-center'>
                 <span className='text-sm text-blue-400'>{date}</span>
                 {category && <div className='text-sm border-2 rounded-xl border-blue-300 text-sky-400 px-2'>{category}</div>}
@@ -22,8 +22,8 @@ const Posts = ({ posts }: PostsProps) => {
                   {tags.map((tag) => <div key={tag} className='text-sm text-blue-800'>#{tag}</div>)}
                 </div>
               )}
-            </li>
-          </Link>
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
