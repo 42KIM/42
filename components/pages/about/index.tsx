c5bb4b811ff027f32daaf72422bd2c5eb68904bd
42KIM <ksahoo524@gmail.com>
2023-06-03 03:52:41 +0900

    test: 운영 환경 확인을 위한 환경변수 테스트 커밋

--- components/pages/about/index.tsx
+++ components/pages/about/index.tsx
@@ -2,10 +2,11 @@ type AboutProps = {
   content: string,
 };
 
+const vercel = process.env.VERCEL;
 const env = process.env.VERCEL_ENV;
 
 const About = ({ content }: AboutProps) => {
-  console.log(env);
+  console.log('vercel: ', vercel, 'env: ', env);
   return (
     <div className="prose prose-sm prose-p:my-1 prose-hr:my-5 prose-ul:my-1" dangerouslySetInnerHTML={{ __html: content }} />
   );
