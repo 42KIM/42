63aa9532fe21ada8c5dc61802da3bb86baca7bd7
42KIM <ksahoo524@gmail.com>
2023-06-03 03:58:36 +0900

    test: 환경 변수 반환값 타입 체크용 커밋

--- components/pages/about/index.tsx
+++ components/pages/about/index.tsx
@@ -6,7 +6,7 @@ const vercel = process.env.VERCEL;
 const env = process.env.VERCEL_ENV;
 
 const About = ({ content }: AboutProps) => {
-  console.log('vercel: ', vercel, 'env: ', env);
+  console.log('vercel: ', vercel, typeof vercel, 'env: ', env);
   return (
     <div className="prose prose-sm prose-p:my-1 prose-hr:my-5 prose-ul:my-1" dangerouslySetInnerHTML={{ __html: content }} />
   );
