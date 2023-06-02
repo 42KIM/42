4598610c1cf91cfd2a48425122a3ad070178021b
42KIM <ksahoo524@gmail.com>
2023-06-03 04:01:17 +0900

    test: 테스트용 커밋 원복

--- components/pages/about/index.tsx
+++ components/pages/about/index.tsx
@@ -2,11 +2,7 @@ type AboutProps = {
   content: string,
 };
 
-const vercel = process.env.VERCEL;
-const env = process.env.VERCEL_ENV;
-
 const About = ({ content }: AboutProps) => {
-  console.log('vercel: ', vercel, typeof vercel, 'env: ', env);
   return (
     <div className="prose prose-sm prose-p:my-1 prose-hr:my-5 prose-ul:my-1" dangerouslySetInnerHTML={{ __html: content }} />
   );
--- next.config.js
+++ next.config.js
@@ -7,8 +7,6 @@ const nextConfig = {
     MONGODB_URI: process.env.MONGODB_URI,
     GITHUB_CLIENT_ID: process.env.NODE_ENV === 'development' && process.env.GITHUB_CLIENT_ID_DEVELOPMENT ||
       process.env.NODE_ENV === 'production' && process.env.GITHUB_CLIENT_ID_PRODUCTION,
-    VERCEL: process.env.VERCEL,
-    VERCEL_ENV: process.env.VERCEL_ENV
   },
   images: {
     domains: [ 'avatars.githubusercontent.com' ],
