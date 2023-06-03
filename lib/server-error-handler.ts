bff12536db76f9ff56cf1d7dbe3001165d793506
42KIM <ksahoo524@gmail.com>
2023-06-03 15:17:05 +0900

    feat: server 컨트롤러 공통 에러 핸들러 함수 추가

--- lib/server-error-handler.ts
+++ lib/server-error-handler.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from 'axios';
 import mongoose from 'mongoose';
 import type { NextApiResponse } from 'next';
 
@@ -9,6 +10,13 @@ export const serverErrorHandler = (error: unknown, res: NextApiResponse) => {
   if (error instanceof mongoose.Error.ValidationError) {
     res.status(400).json({
       message: error.message,
+    });
+    return;
+  }
+
+  if (error instanceof AxiosError) {
+    res.status(error.response?.status || 500).json({
+      message: error.response?.data.message || error.message,
     });
     return;
   }
