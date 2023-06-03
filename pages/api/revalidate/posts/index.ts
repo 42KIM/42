119bbffb6feba10174884cd7775c24b3be361fe3
42KIM <ksahoo524@gmail.com>
2023-06-03 15:45:29 +0900

    refactor: revalidation 권한 없음 에러 추가 및 error hof 적용

--- pages/api/revalidate/posts/index.ts
+++ pages/api/revalidate/posts/index.ts
@@ -1,16 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { withErrorHandler } from '@/lib/server-error-handler';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { admin } = req.cookies;
 
   if (admin !== 'true') {
-    res.status(401).json({ message: 'Unauthorized user' });
+    throw new Error('Unauthorized user. Revalidation Not Allowed.');
   }
 
-  try {
-    await res.revalidate('/posts');
-    res.status(200).json({ revalidated: true });
-  } catch (err) {
-    res.status(500).send('Error revalidating');
-  }
+  await res.revalidate('/posts');
+  res.status(200).json({ revalidated: true });
 }
+
+export default withErrorHandler(handler);
