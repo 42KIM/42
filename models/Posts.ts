f1a345f0fdcc0cc7656ec833536217b9a26700ee
42KIM <ksahoo524@gmail.com>
2023-06-05 17:22:25 +0900

    feat: noImplicitAnys 활성화로 인해 api payload 모델 타입 추가

--- models/Posts.ts
+++ models/Posts.ts
@@ -12,5 +12,8 @@ const PostsSchema = new Schema({
 });
 
 export type Post = InferSchemaType<typeof PostsSchema> & { _id: string };
+export type PostCreateRequest = Omit<Post, '_id' | 'createdAt' | 'updatedAt'>;
+export type PostUpdateRequest = Partial<PostCreateRequest> & { _id: string };
+export type PostDeleteRequest = Pick<Post, '_id'>;
 
 export default mongoose.models.Posts || mongoose.model('Posts', PostsSchema);
