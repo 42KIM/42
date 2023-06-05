dfd70481db5fc0d45195004cef637c4e2ddad30d
42KIM <ksahoo524@gmail.com>
2023-06-05 17:22:47 +0900

    feat: apiService에 payload 타입 적용

--- apis/index.ts
+++ apis/index.ts
@@ -1,21 +1,23 @@
 import { axiosInstance as _request } from '@/lib/axios';
+import type { CommentCreateRequest, CommentDeleteRequest, CommentUpdateRequest, CommentLikeRequest } from '@/models/Comments';
+import type { PostCreateRequest, PostDeleteRequest, PostUpdateRequest } from '@/models/Posts';
 
 export const APIService = {
-  createPosts: async (payload) => {
+  createPosts: async (payload: PostCreateRequest) => {
     await _request({
       method: 'POST',
       url: '/api/posts',
       data: payload,
     });
   },
-  deletePosts: async (payload) => {
+  deletePosts: async (payload: PostDeleteRequest) => {
     await _request({
       method: 'DELETE',
       url: '/api/posts',
       data: payload,
     });
   },
-  updatePosts: async (payload) => {
+  updatePosts: async (payload: PostUpdateRequest) => {
     await _request({
       method: 'PUT',
       url: '/api/posts',
@@ -28,14 +30,14 @@ export const APIService = {
       url: '/api/revalidate/posts',
     });
   },
-  revalidatePostsDetail: async (payload) => {
+  revalidatePostsDetail: async (payload: { id: string }) => {
     await _request({
       method: 'POST',
       url: '/api/revalidate/posts/detail',
       data: payload,
     });
   },
-  getComments: async (payload) => {
+  getComments: async (payload: string) => {
     const { data } = await _request({
       method: 'GET',
       url: '/api/comments',
@@ -46,35 +48,35 @@ export const APIService = {
 
     return data;
   },
-  createComments: async (payload) => {
+  createComments: async (payload: CommentCreateRequest) => {
     await _request({
       method: 'POST',
       url: '/api/comments',
       data: payload,
     });
   },
-  updateComments: async (payload) => {
+  updateComments: async (payload: CommentUpdateRequest) => {
     await _request({
       method: 'PUT',
       url: '/api/comments',
       data: payload,
     });
   },
-  deleteComments: async (payload) => {
+  deleteComments: async (payload: CommentDeleteRequest) => {
     await _request({
       method: 'DELETE',
       url: '/api/comments',
       data: payload,
     });
   },
-  likeComments: async (payload) => {
+  likeComments: async (payload: CommentLikeRequest) => {
     await _request({
       method: 'PUT',
       url: '/api/comments/likes',
       data: payload,
     });
   },
-  createToken: async (payload) => {
+  createToken: async (payload: { code: string }) => {
     await _request({
       method: 'POST',
       url: '/api/auth/token-create',
