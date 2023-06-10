841a2b9f0b24978e93dfa10f7f0506142506f37a
42KIM <ksahoo524@gmail.com>
2023-06-11 01:03:03 +0900

    chore: 댓글 좋아요 비로그인 클릭 시 문구 변경

--- components/pages/posts/post/comment-section/CommentCard.tsx
+++ components/pages/posts/post/comment-section/CommentCard.tsx
@@ -158,8 +158,8 @@ const CommentCard = ({
                 userName: user.login,
               })
               : showDialog({
-                title: '주의',
-                content: '로그인이 필요합니다.',
+                title: '알림',
+                content: '좋아요를 누르려면 로그인이 필요합니다.',
               });
           }}
         >좋아요 {likes?.length}</button>
