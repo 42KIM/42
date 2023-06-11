5960ad00eb0a9a2258b92970dbcfb8172f979855
42KIM <ksahoo524@gmail.com>
2023-06-11 16:41:51 +0900

    feat: 포스트 좋아요 Likes model 추가

---
+++ models/Likes.ts
@@ -0,0 +1,24 @@
+import type { InferSchemaType } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+
+interface ILikeAuthor {
+  author: string;
+  authorId: number;
+}
+
+interface ILikes extends mongoose.Document {
+  postId: string;
+  authors: ILikeAuthor[];
+}
+
+const LikesSchema = new Schema<ILikes>({
+  postId: { type: String, required: true },
+  authors: { type: [ { author: String, authorId: Number } ], required: true },
+}, {
+  timestamps: true,
+});
+
+export type Likes = InferSchemaType<typeof LikesSchema>;
+export type PostsLikeRequest = Pick<Likes, 'postId' | 'authors'>;
+
+export default mongoose.models.Likes || mongoose.model('Likes', LikesSchema);
