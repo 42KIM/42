22c44adb1ad1d8729533160def48e1bafbabb2e3
42KIM <ksahoo524@gmail.com>
2023-06-24 17:35:15 +0900

    style: 모바일 사용 편의성 해결을 위한 스타일 수정

--- components/pages/home/index.tsx
+++ components/pages/home/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Home = () => {
   return (
-    <div className="fixed left-0 top-0 bottom-0 w-full flex flex-col justify-center items-center">
+    <div className="fixed left-0 top-0 bottom-0 w-full flex flex-col justify-center items-center gap-4">
       <Link href="/about">
         <div className="hover:scale-125 hover:text-blue-400">ABOUT</div>
       </Link>
