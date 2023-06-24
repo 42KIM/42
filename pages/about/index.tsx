f290d3113320acbc632f571c26c95aee7292a760
42KIM <ksahoo524@gmail.com>
2023-06-24 17:40:45 +0900

    chore: 이력서 마크다운 파일 import 경로 수정

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,7 +4,7 @@ import fs from 'fs/promises';
 export { default } from '@/components/pages/about';
 
 export const getStaticProps = async () => {
-  const resumeMarkdown = await fs.readFile('resume.md', 'utf-8');
+  const resumeMarkdown = await fs.readFile('public/resume.md', 'utf-8');
   const resumeHTML = parseMarkdownToHTML(resumeMarkdown);
 
   return {
